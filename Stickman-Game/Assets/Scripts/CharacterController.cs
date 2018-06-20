8a3467afa07f4d65cae46104c875510869647e88
Melih <mlhtncc@gmail.com>
2018-06-20 20:10:27 +0300

    Character Controller added

---
+++ Stickman-Game/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float speed = 3f;
+
+	// Use this for initialization
+	void Start ()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0f);	
+    }
+}
