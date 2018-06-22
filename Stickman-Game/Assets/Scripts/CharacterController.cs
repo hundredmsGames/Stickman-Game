49cecbd8d7ea3690cd74d10efabfcf51a2f686e1
Melih <mlhtncc@gmail.com>
2018-06-22 15:30:24 +0300

    Line, Character Collision

--- Stickman-Game/Assets/Scripts/CharacterController.cs
+++ Stickman-Game/Assets/Scripts/CharacterController.cs
@@ -6,19 +6,36 @@ public class CharacterController : MonoBehaviour
 {
     public float speed = 3f;
 
-    private Rigidbody2D rigidBody;
+    private Rigidbody2D rigidbody;
+
+    bool mouseUp;
 
 	// Use this for initialization
 	void Start ()
     {
-        rigidBody = GetComponent<Rigidbody2D>();
+        rigidbody = GetComponent<Rigidbody2D>();
       
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        rigidBody.velocity = new Vector2(speed, rigidBody.velocity.y);
+        rigidbody.velocity = new Vector2(speed, rigidbody.velocity.y);
+
+        if (Input.GetMouseButtonUp(0))
+            mouseUp = true;
         
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("hi");
+        //if (mouseUp)
+        //{
+            rigidbody.constraints = RigidbodyConstraints2D.FreezePositionY | RigidbodyConstraints2D.FreezePositionX |
+                        RigidbodyConstraints2D.FreezeRotation;
+
+            mouseUp = false;
+        //}
     }
 }
