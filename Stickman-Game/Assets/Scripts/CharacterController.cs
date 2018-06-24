a76b242b4c7fab6eaf4e811b7eb27f566e292554
Melih <mlhtncc@gmail.com>
2018-06-24 15:37:12 +0300

    Code cleaning, optimization

--- Stickman-Game/Assets/Scripts/CharacterController.cs
+++ Stickman-Game/Assets/Scripts/CharacterController.cs
@@ -4,11 +4,6 @@ using UnityEngine;
 
 public partial class CharacterController : MonoBehaviour
 {
-    public GameObject topAngledRayPoint;
-    public GameObject horizontalTop;
-    public GameObject horizontalMiddle;
-    public GameObject horizontalBottom;
-
     private Animator animator;
     private Rigidbody2D rigidBody;
     public DrawLine2D drawLine;
@@ -16,17 +11,13 @@ public partial class CharacterController : MonoBehaviour
     public float speed;
     bool triggered;
 
-    public float rayLength = 10f;
-
-    // TODO: Find a better name for me.
-    Vector2 angledVector;
+    
 
 
     private void Start ()
     {
         rigidBody = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-        angledVector = RotateRay(topAngledRayPoint,180, rayLength);        
         
         // Run
         animator.SetFloat("Speed", 1f);
@@ -35,7 +26,7 @@ public partial class CharacterController : MonoBehaviour
     private void Update ()
     {
         CheckLine();
-        CheckRaysRENAME();
+        CheckRays();
     }
 
     private void FixedUpdate()
@@ -79,40 +70,5 @@ public partial class CharacterController : MonoBehaviour
                 drawLine.StopDrawing(true);
             }
         }
-    }
-
-    private void CheckRays()
-    {
-        //for debug
-        Debug.DrawLine(topAngledRayPoint.transform.position, RotateRay(topAngledRayPoint, 90, rayLength), Color.red);
-        Debug.DrawLine(topAngledRayPoint.transform.position, RotateRay(topAngledRayPoint, -45f, rayLength), Color.blue);
-        Debug.DrawLine(horizontalTop.transform.position, RotateRay(horizontalTop, 0, rayLength), Color.green);
-        Debug.DrawLine(horizontalMiddle.transform.position, RotateRay(horizontalMiddle, 0, rayLength), Color.white);
-        Debug.DrawLine(horizontalBottom.transform.position, RotateRay(horizontalBottom, 0, rayLength), Color.yellow);
-
-        RaycastHit2D raycastHit2D = Physics2D.Linecast(topAngledRayPoint.transform.position, Vector2.up);
-        if (/*charachter is crouching or crawling and*/ raycastHit2D.collider != null)
-        {
-
-        }
-        raycastHit2D = Physics2D.Linecast(topAngledRayPoint.transform.position, RotateRay(topAngledRayPoint, 45f, rayLength));
-
-        raycastHit2D = Physics2D.Raycast(topAngledRayPoint.transform.position, Vector2.up, 10);
-        if (/*charachter is crouching or crawling and*/ raycastHit2D.collider != null)
-        {
-
-        }
-        raycastHit2D = Physics2D.Raycast(topAngledRayPoint.transform.position, angledVector, 10);
-    }
-
-    private Vector2 RotateRay(GameObject go, float angle, float length)
-    {
-        Vector2 v = new Vector2(length, 0);
-        float newX = v.x * Mathf.Cos(angle * Mathf.Deg2Rad) + v.y * -Mathf.Sin(angle * Mathf.Deg2Rad);
-        float newY = v.x * Mathf.Sin(angle * Mathf.Deg2Rad) + v.y * Mathf.Cos(angle * Mathf.Deg2Rad);
-
-        newX += go.transform.position.x;
-        newY += go.transform.position.y;
-        return new Vector2(newX, newY);
     }
 }
---
+++ Stickman-Game/Assets/Scripts/CharacterRayController.cs
@@ -0,0 +1,136 @@
+﻿using UnityEngine;
+
+public partial class CharacterController : MonoBehaviour
+{
+    public Transform verticalRayPoint;
+    public Transform horTopRayPoint;
+    public Transform horMidRayPoint;
+    public Transform horBottomRayPoint;
+
+    public float rayLength;
+    public float rayAngle;
+    
+    void CheckRays()
+    {
+        // Vertical ray, (towards up)
+        RaycastHit2D verRaycastHit = Physics2D.Raycast(verticalRayPoint.position, Vector2.up * rayLength);
+        Debug.DrawRay(verticalRayPoint.position, Vector2.up * rayLength, Color.red);
+
+        // Horizontal-Top ray
+        RaycastHit2D horTopRaycastHit = Physics2D.Raycast(horTopRayPoint.position, Vector2.right * rayLength);
+        Debug.DrawRay(horTopRayPoint.position, Vector2.right * rayLength, Color.green);
+
+        // Horizontal-Middle ray
+        RaycastHit2D horMidRaycastHit = Physics2D.Raycast(horMidRayPoint.position, Vector2.right * rayLength);
+        Debug.DrawRay(horMidRayPoint.position, Vector2.right * rayLength, Color.white);
+
+        // Horizontal-Bottom ray
+        RaycastHit2D horBottomRaycastHit = Physics2D.Raycast(horBottomRayPoint.position, Vector2.right * rayLength);
+        Debug.DrawRay(horBottomRayPoint.position, Vector2.right * rayLength, Color.yellow);
+
+        // Angled Ray (for empty spaces)
+        Vector2 rayEndPoint = RotateRay(verticalRayPoint, rayAngle, rayLength);
+        RaycastHit2D angledRaycastHit = Physics2D.Linecast(verticalRayPoint.position, rayEndPoint);
+        Debug.DrawLine(verticalRayPoint.position, rayEndPoint, Color.blue);
+
+
+        if (/*charachter is crouching or crawling and*/ verRaycastHit.collider != null)
+        {
+            //keep crawling or crouching -- keep doing what you're doing
+        }
+
+        //we have empty space decide what to do
+        //after we decide player can change his mind
+        if (angledRaycastHit.collider == null)
+        {
+            //there ise a gap ready to jump over
+            Debug.Log("jump over gap");
+        }
+
+
+        //we have 3 line casts for checking either we have somethin on our way like obstacles that we can jump over 
+        //that we can climb or we can crouch or crawl
+        //if there is something on top so we can crouch maybe
+        if (horTopRaycastHit.collider != null)
+        {
+            //if there  is something in the middle so we can crawl maybe
+            if (horMidRaycastHit.collider != null)
+            {
+                //if there is something in bottom too so we can try to climb
+                if (horBottomRaycastHit.collider != null)
+                {
+                    //climb (running on wall)
+                    Debug.Log("climb (running on wall)");
+                }
+                else
+                {
+                    //crawl
+                    Debug.Log("crawl");
+                }
+            }
+            //if there is nothing in middle
+            else
+            {
+                //if there is something in bottom too so we can try to climb
+                if (horBottomRaycastHit.collider != null && horBottomRaycastHit.collider.tag != "Character")
+                {
+                    Debug.Log("jump between bottom and top");
+                }
+                else
+                {
+                    Debug.Log("crouch");
+                }
+            }
+        }
+        else
+        {
+            //if there  is something in the middle so we can jump and climb
+            if (horMidRaycastHit.collider != null)
+            {
+                float randomNumber = Random.Range(0f, 1f);
+
+                //if there is something in bottom too so we can try to climb
+                if (horBottomRaycastHit.collider != null)
+                {
+                    //climb jump
+                    Debug.Log("climb (jump)");
+                }
+                else if (randomNumber < 0.5)
+                {
+                    //crawl
+                    Debug.Log("crawl");
+
+                }
+                else
+                {
+                    // climb Jump
+                    Debug.Log("climb (jump)");
+                }
+            }
+            else
+            {
+                //if there is something in bottom too so we can try to climb
+                if (horBottomRaycastHit.collider != null)
+                {
+                    //jump over bottom
+                    Debug.Log("jump over bottom");
+                }
+                else
+                {
+                    //walk trough
+                }
+            }
+        }
+    }
+
+    private Vector2 RotateRay(Transform t, float angle, float length)
+    {
+        Vector2 v = new Vector2(length, 0);
+        float newX = v.x * Mathf.Cos(angle * Mathf.Deg2Rad) + v.y * -Mathf.Sin(angle * Mathf.Deg2Rad);
+        float newY = v.x * Mathf.Sin(angle * Mathf.Deg2Rad) + v.y * Mathf.Cos(angle * Mathf.Deg2Rad);
+
+        newX += t.position.x;
+        newY += t.position.y;
+        return new Vector2(newX, newY);
+    }
+}
