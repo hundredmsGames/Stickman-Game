a8310c96e879403beb39697238ac6e3cd916938c
SOFTSAM <sml.ozclk@gmail.com>
2018-07-05 18:10:34 +0300

    animation checker added

---
+++ Stickman-Game/Assets/AnimationChecker.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationChecker : MonoBehaviour {
+
+    public static AnimationChecker Instance;
+    public bool JumpOverAnimationEnded { get; set; }
+                                       // Use this for initialization
+    void Start () {
+        if (Instance != null)
+            return;
+        Instance = this;
+        JumpOverAnimationEnded = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+   
+    public void JumpOverObstacleAnimationStarted()
+    {
+        JumpOverAnimationEnded = false;
+        Debug.Log("JumpOverObstacleAnimationStarted");
+    }
+    public void JumpOverObstacleAnimationEnded()
+    {
+        JumpOverAnimationEnded = true;
+        Debug.Log("JumpOverObstacleAnimationEnded");
+
+    }
+}
